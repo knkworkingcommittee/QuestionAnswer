34007becb0897b3b58e4ed7e80b9612bfa033ffc
knkworkingcommittee <knkworkingcommittee@gmail.com>
2025-08-04 17:45:19 +0000

    name changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -80,7 +80,7 @@ const Index = () => {
       <div className="max-w-5xl mx-auto">
         <div className="text-center mb-10">
           <h1 className="text-5xl font-extrabold text-[#8B5CF6] animate-pulse tracking-tight">
-            Blood and Bugs 2025
+            $ave Neirons & Nephrons 2025
           </h1>
         </div>
         <div className="text-center mb-16 animate-fade-in">
